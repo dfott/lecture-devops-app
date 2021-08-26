ba8ae0cbf530066c99cd09def058bc8183f57608
Daniel Fott <daniel.fott@senacor.com>
2021-08-26 23:28:57 +0200

    Change header name to prod

--- app/client/src/components/Header/Header.js
+++ app/client/src/components/Header/Header.js
@@ -5,7 +5,7 @@ import './Header.css'
 export default function Header() {
     return (
         <MDBContainer className="header-main">
-            ToDo App - Dev
+            ToDo App - Prod
         </MDBContainer>
     )
 }
